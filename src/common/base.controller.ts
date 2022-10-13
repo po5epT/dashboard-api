7d0afd14dc7a28fcf2f1489883e23f2c1d56fcc8
po5epT <megastorm87@mail.ru>
2022-10-13 23:55:44 +0400

    Adds IRouteController interface and BaseController class

---
+++ src/common/base.controller.ts
@@ -0,0 +1,38 @@
+import { LoggerService } from '../logger/logger.service';
+import { Response, Router } from 'express';
+import { IControllerRoute } from './route.interface';
+
+export abstract class BaseController {
+  private readonly _router: Router;
+
+  constructor(private logger: LoggerService) {
+    this._router = Router();
+  }
+
+  get router() {
+    return this._router;
+  }
+
+  public send<T>(res: Response, code: number, message: T) {
+    res.type('application/json');
+    res.status(code);
+
+    return res.json(message);
+  }
+
+  public ok<T>(res: Response, message: T) {
+    return this.send<T>(res, 200, message);
+  }
+
+  public created(res: Response) {
+    return res.sendStatus(201);
+  }
+
+  protected bindRoutes(routes: IControllerRoute[]) {
+    for(const route of routes) {
+      this.logger.log(`[${route.method}] ${route.path}`);
+      const boundHandler = route.handler.bind(this);
+      this.router[route.method](route.path, boundHandler);
+    }
+  }
+}
